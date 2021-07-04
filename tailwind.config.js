64e40fa330b220fe649d8716cbf634c8f30be49b
Christoph von Gellhorn <cvgellhorn@gmail.com>
2021-07-04 19:44:10 +1000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
